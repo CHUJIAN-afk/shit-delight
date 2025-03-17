0edacf5c26f71f6effa10f9db90dffee3dc7605b
yiran1457 <3270897489@qq.com>
2025-03-17 22:01:11 +0800

    修改物品耐久

--- startup_scripts/start.js
+++ startup_scripts/start.js
@@ -16,3 +16,21 @@ StartupEvents.registry("item",event=>{
     .maxStackSize(1)
     .displayName("便携胃镜")
 })
+let $ArmorItem = Java.loadClass("net.minecraft.world.item.ArmorItem")
+const WEA = [
+    "farmersdelight:diamond_knife", "farmersdelight:netherite_knife",
+    "farmersdelight:iron_knife", "farmersdelight:golden_knife",
+    "farmersdelight:skillet", "farmersdelight:flint_knife",
+    "twilightdelight:fiery_knife", "twilightdelight:knightmetal_knife",
+    "twilightdelight:ironwood_knife", "twilightdelight:steeleaf_knife",
+    "ends_delight:dragon_tooth_knife", "ends_delight:end_stone_knife",
+    "ends_delight:dragon_egg_shell_knife", "ends_delight:purpur_knife"
+];
+ItemEvents.modification(event => {
+    Ingredient.all.getItemIds().forEach(id => {
+        if (Item.of(id).item.maxDamage > 0 && WEA.indexOf(id) == -1&&!(Item.of(id).item instanceof $ArmorItem))
+            event.modify(id, bd => {
+                bd.setMaxDamage(1)
+            })
+    })
+})
