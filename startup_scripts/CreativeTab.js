19f0a01f553bcb846427c48bd0db4e7735801de0
CHUJIAN-afk <liuhong.bing@qq.com>
2025-03-19 17:03:15 +0800

    增加创造物品栏图标

--- startup_scripts/CreativeTab.js
+++ startup_scripts/CreativeTab.js
@@ -1,24 +1,25 @@
-const createTabItems = tag => 
+const createTabItems = tag =>
     Ingredient.of(tag).getItemIds().map(id => Item.of(id));
 const tabConfigs = [
-    { suffix: 'QYGM', tag: '#minecraft:FSNJ', displayKey: '潜渊共鸣' },
-    { suffix: 'BZQY', tag: '#brewinandchewin:fermented_drinks', displayKey: '莓酿离歌' },
-    { suffix: 'GXZM', tag: '#minecraft:GXZM', displayKey: '果香织梦' },
-    { suffix: 'XKHX', tag: '#minecraft:XKHX', displayKey: '虚空遗尘' },
-    { suffix: 'JHXY', tag: '#minecraft:JHXY', displayKey: '匠魂飨宴' },
-    { suffix: 'SLMY', tag: '#minecraft:SLMY', displayKey: '森灵秘语' },
-    { suffix: 'FZQY', tag: '#minecraft:JFZS', displayKey: '风之轻语' },
-    { suffix: 'PSZG', tag: '#minecraft:PSZG', displayKey: '磐石之根' },
-    { suffix: 'FSSX', tag: '#minecraft:shit', displayKey: '腐嗅噬心' },
-    { suffix: 'XHRL', tag: '#minecraft:XHRL', displayKey: '星火熔炉' },
-    { suffix: 'TYGY', tag: '#minecraft:TYGY', displayKey: '餮魇归一' },
-    { suffix: 'YHKZ', tag: '#minecraft:YHKZ', displayKey: '腌痕铠胄' }
+    { suffix: 'QYGM', tag: '#minecraft:FSNJ', displayKey: 'silentsdelight:sculk_shrieker_shake' },
+    { suffix: 'BZQY', tag: '#brewinandchewin:fermented_drinks', displayKey: 'brewinandchewin:beer' },
+    { suffix: 'GXZM', tag: '#minecraft:GXZM', displayKey: 'fruitsdelight:lychee_jelly' },
+    { suffix: 'XKHX', tag: '#minecraft:XKHX', displayKey: 'ends_delight:ender_noodle' },
+    { suffix: 'JHXY', tag: '#minecraft:JHXY', displayKey: 'brewinandchewin:vegetable_omelet' },
+    { suffix: 'SLMY', tag: '#minecraft:SLMY', displayKey: 'fruitsdelight:blueberry' },
+    { suffix: 'FZQY', tag: '#minecraft:JFZS', displayKey: 'miners_delight:glow_squid' },
+    { suffix: 'PSZG', tag: '#minecraft:PSZG', displayKey: 'farmersdelight:smoked_ham' },
+    { suffix: 'FSSX', tag: '#minecraft:shit', displayKey: 'kubejs:shit' },
+    { suffix: 'XHRL', tag: '#minecraft:XHRL', displayKey: 'mynethersdelight:spicy_skewer' },
+    { suffix: 'TYGY', tag: '#minecraft:TYGY', displayKey: 'twilightdelight:aurora_pie_slice' },
+    { suffix: 'YHKZ', tag: '#minecraft:YHKZ', displayKey: 'vintagedelight:century_egg' }
 ];
-
 StartupEvents.registry('creative_mode_tab', event => {
     tabConfigs.forEach(config => {
         event.create(`shit_delight_${config.suffix}`)
+            .icon(() => Item.of(config.displayKey))
             .displayName(Text.translate(`shit.delight.${config.suffix}`))
-            .content(() => createTabItems(config.tag));
+            .content(() => createTabItems(config.tag))
+
     });
 });
