fa5a3b5c00edd9b2ee5ddb5d0dd3cf25ae495cbb
yiran1457 <3270897489@qq.com>
2025-03-23 18:00:25 +0800

    feat(kubejs): 优化属性界面渲染效果
    - 添加屏幕打开事件处理，重置开放进度
    - 实现属性界面的动态渲染效果，包括文本和颜色渐变
    - 优化线条绘制逻辑，支持动态展开效果

--- kubejs/client_scripts/yiran/screen.js
+++ kubejs/client_scripts/yiran/screen.js
@@ -1,5 +1,18 @@
 let $ScreenEvent$Render$Post = Java.loadClass("net.minecraftforge.client.event.ScreenEvent$Render$Post")
+let $ScreenEvent$Opening = Java.loadClass("net.minecraftforge.client.event.ScreenEvent$Opening")
+let openTick = 0
+NativeEvents.onEvent($ScreenEvent$Opening, event => {
+    switch (Number(event.screen.id)) {
+        case 101:
+            openTick = 0
+            break
+    }
 
+})
+
+let attributeCount = 6
+let list = ["XKHX", "JHXY", "SLMY", "XHRL", "JFZS", "PSZG"]
+let rot = new Quaternionf().rotateZ(2 * KMath.PI / attributeCount)
 NativeEvents.onEvent($ScreenEvent$Render$Post, event => {
     let { guiGraphics, screen } = event
     if (screen.id != 101) return
@@ -7,17 +20,54 @@ NativeEvents.onEvent($ScreenEvent$Render$Post, event => {
     {
         poseStack.pushPose()
         poseStack.translate(Client.window.guiScaledWidth / 2, Client.window.guiScaledHeight / 2, 0)
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, 100, 0, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, -100, 0, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, 50, 88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, 50, -88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, -50, 88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
-        ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, -50, -88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        guiGraphics.fill(-200, -150, 200, 150, ShitDelightClientUtils.RGBA(114, 114, 114, 114))
+        openTick < 1 && (openTick += openTick * 0.01 + 0.01)
+        list.forEach((value, index) => {
+            guiGraphics["drawString(net.minecraft.client.gui.Font,java.lang.String,float,float,int,boolean)"](
+                Client.font, Text.translatable('shit.delight.' + value).getString(),
+                Math.cos(index * KMath.PI / 3) * 120 - 20, Math.sin(index * KMath.PI / 3) * 120 - 4,
+                -1, false
+            )
+        })
+        poseStack.pushPose()
+        {
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(170, 0, 170, 255))
+            poseStack.mulPose(rot)
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(255, 170, 0, 255))
+            poseStack.mulPose(rot)
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(0, 170, 0, 255))
+            poseStack.mulPose(rot)
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(255, 85, 85, 255))
+            poseStack.mulPose(rot)
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(85, 255, 255, 255))
+            poseStack.mulPose(rot)
+            guiGraphics.fill(2, -1, 2 + openTick * 100, 1, ShitDelightClientUtils.RGBA(85, 85, 85, 255))
+        }
+        poseStack.popPose()
+
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(42 * 2.55, 0 * 2.55, 42 * 2.55, 255))
+        // poseStack.mulPose(rot)
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(42 * 2.55, 42 * 2.55, 0 * 2.55, 255))
+        // poseStack.mulPose(rot)
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(0 * 2.55, 42 * 2.55, 0 * 2.55, 255))
+        // poseStack.mulPose(rot)
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(63 * 2.55, 21 * 2.55, 21 * 2.55, 255))
+        // poseStack.mulPose(rot)
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(21 * 2.55, 63 * 2.55, 63 * 2.55, 255))
+        // poseStack.mulPose(rot)
+        // guiGraphics.fill(0, -1, openTick * 100, 1, ShitDelightClientUtils.RGBA(21 * 2.55, 21 * 2.55, 21 * 2.55, 255))
+
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*100, openTick*0, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*-100, openTick*0, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*50, openTick*88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*50, openTick*-88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*-50, openTick*88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
+        // ShitDelightClientUtils.drawLine(guiGraphics, poseStack, 0, 0, openTick*-50, openTick*-88, 2, ShitDelightClientUtils.RGBA(255,0,255,255))
         poseStack.popPose()
     }
 })
 
-if(false)
-ItemEvents.firstLeftClicked(e => {
-    Client.setScreen(global.createNewScreen(null, 101, 'test'))
-})
+if (false)
+    ItemEvents.firstLeftClicked(e => {
+        Client.setScreen(global.createNewScreen(null, 101, 'test'))
+    })
